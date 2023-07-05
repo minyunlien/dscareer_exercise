9bc3fbf70cc1bb9798caeca0c12b5d5ab147745d
minyunlien <lienmy811@gmail.com>
2023-07-06 01:36:17 +0800

    Create a new folder for basic class

---
+++ basic01/task02.py
@@ -0,0 +1,20 @@
+# Task #02 作業 02【實作題】兩數相加
+
+x = input('input a number x: ') 
+y = input('input a number y: ' )
+
+sum = x+y
+
+print(sum)
+
+
+# Task #02 作業 03【實作題】變數交換
+
+
+a = input('input the first integer: ') 
+b = input('input the second integer: ')
+print(a, b) # 5, 10
+
+a, b = b, a
+
+print(a, b) # 10, 5
